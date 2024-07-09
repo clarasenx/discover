c23cec949aef7d6a416b7f1b1d4ce3b4c43192de
Marcelo Pinheiro de Moraes <marcelo.moraes@PORTOVELHO.RO.GOV.BR>
2024-07-09 12:27:41 -0400

    add js code

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+    const html = document.documentElement
+    const img = document.querySelector('#profile img');    
+    
+    html.classList.toggle('light')
+    
+    if(html.classList.contains('light')) {
+        img.setAttribute('src', '/assets/avatar-light.png');
+    } else {
+        img.setAttribute('src', '/assets/avatar.png');
+    }
+
+    if(html.classList.contains('light')) {
+        img.setAttribute('alt', 'foto de um homem adulto sorrindo, usando óculos escuros e jaqueta cinza escuro');
+    } else {
+        img.setAttribute('alt', 'foto de um homem adulto sorrindo, usando óculos e camisa preta');
+    }
+}
